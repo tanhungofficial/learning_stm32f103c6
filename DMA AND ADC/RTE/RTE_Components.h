ba6dae2ee9cfceb94f114ef3b9520d7a9eb48117
hungnt <tanhungofficial@gmail.com>
2019-12-17 14:34:56 +0700

    dma_adc

---
+++ DMA AND ADC/RTE/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DMA_ADC' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_ADC
+#define RTE_DEVICE_STDPERIPH_DMA
+#define RTE_DEVICE_STDPERIPH_EXTI
+#define RTE_DEVICE_STDPERIPH_FLASH
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_IWDG
+#define RTE_DEVICE_STDPERIPH_PWR
+#define RTE_DEVICE_STDPERIPH_RCC
+
+#endif /* RTE_COMPONENTS_H */
